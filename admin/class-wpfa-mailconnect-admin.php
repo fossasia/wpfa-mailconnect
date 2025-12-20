ab64d4292bb6e8fecd437af670f374b6db4e6503
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-12-20 20:33:39 +0600

    docs: align since versions with 1.2.0

--- admin/class-wpfa-mailconnect-admin.php
+++ admin/class-wpfa-mailconnect-admin.php
@@ -122,7 +122,7 @@ class Wpfa_Mailconnect_Admin {
 	/**
 	 * Clears the decryption failure flag when settings are updated.
 	 *
-	 * @since    1.2.2
+	 * @since    1.2.0
 	 */
 	public function clear_decryption_failure_flag() {
 		$screen = get_current_screen();
